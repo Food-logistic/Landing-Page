789112d2360bcd00d7fa2d4e3101d3bba11bf763
MaricarmenOrtiz <135289579+MaricarmenOrtiz@users.noreply.github.com>
2023-06-26 18:46:55 -0500

    Add files via upload

---
+++ js/indexjs.js
@@ -0,0 +1,56 @@
+
+function abrirmenu(){
+    document.getElementById("menu").style.display="block";
+}
+
+function cerrarmenu(){
+    document.getElementById("menuabierto").style.display="none";
+}
+function cerrarmenu(){
+    document.getElementById("menu").style.display="none";
+}
+
+function abrir(){
+    document.getElementById("vent").style.display="block";
+}
+
+function cerrar(){
+    document.getElementById("vent").style.display="none";
+}
+
+//VENTANAS DE FUNCIONALIDADES
+//ESTADISTICAS
+function abrirestadisticas(){
+    document.getElementById("estadisticas").style.display="block";
+}
+
+function cerrarestadisticas(){
+    document.getElementById("estadisticas").style.display="none";
+}
+
+//INVENTARIO
+function abririnventarios(){
+    document.getElementById("inventarios").style.display="block";
+}
+
+function cerrarinventarios(){
+    document.getElementById("inventarios").style.display="none";
+}
+//MULTIPLATAFORMA
+
+function abrirmultiplataforma(){
+    document.getElementById("multiplataforma").style.display="block";
+}
+
+function cerrarmultiplataforma(){
+    document.getElementById("multiplataforma").style.display="none";
+}
+
+//RECOMENDADCIONES
+function abrirrecomendaciones(){
+    document.getElementById("recomendaciones").style.display="block";
+}
+
+function cerrarrecomendaciones(){
+    document.getElementById("recomendaciones").style.display="none";
+}
